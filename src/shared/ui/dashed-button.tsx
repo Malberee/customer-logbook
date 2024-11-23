3a96751cdcf0393b4a03f7d91da9b82cf99444c7
Malberee <82259650+Malberee@users.noreply.github.com>
2024-11-23 15:49:19 +0200

    fix(dashed-button): props

--- src/shared/ui/dashed-button.tsx
+++ src/shared/ui/dashed-button.tsx
@@ -1,18 +1,15 @@
-import type { FC, ReactNode } from 'react'
+import type { ButtonHTMLAttributes, FC } from 'react'
 
 import { cn } from '@shared/lib'
 
-interface DashedButtonProps {
-  children?: ReactNode
-  className?: string
-}
-
-export const DashedButton: FC<DashedButtonProps> = ({
+export const DashedButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
   children,
   className,
+  ...props
 }) => {
   return (
     <button
+      {...props}
       className={cn(
         'flex w-full items-center justify-center rounded-md border border-dashed border-sky-500 bg-sky-500/20 p-4 font-medium text-sky-500 backdrop-blur-md transition-transform active:scale-[0.98]',
         className,
