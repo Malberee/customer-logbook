40cd3db2a87ab656f9ced2b7962f8b902a21e531
Malberee <82259650+Malberee@users.noreply.github.com>
2024-11-27 15:58:12 +0200

    feat: implement clearable search

--- src/features/search/ui.tsx
+++ src/features/search/ui.tsx
@@ -1,4 +1,4 @@
-import { Search } from 'lucide-react'
+import { CircleX, Search } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import { useDebounce } from 'use-debounce'
 
@@ -16,12 +16,21 @@ export const SearchBar = () => {
 
   return (
     <div className="relative ml-auto max-w-[300px] flex-1">
+      <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground" />
       <Input
         onChange={(e) => setValue(e.target.value)}
-        className="pr-12"
+        value={value}
+        className="pl-12 pr-10"
         placeholder="Search"
       />
-      <Search className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground" />
+      {value ? (
+        <button
+          className="absolute right-3 top-1/2 -translate-y-1/2"
+          onClick={() => setValue('')}
+        >
+          <CircleX className="size-4 text-muted-foreground transition-opacity hover:cursor-pointer hover:opacity-80" />
+        </button>
+      ) : null}
     </div>
   )
 }
