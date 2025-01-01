1557e4a455b3b78541c43d667a00cc6673f1ef82
Malberee <82259650+Malberee@users.noreply.github.com>
2025-01-01 16:15:23 +0200

    fix: close sheet on navigate

--- src/widgets/sidebar/ui.tsx
+++ src/widgets/sidebar/ui.tsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from '@tanstack/react-router'
 import { BookUser, ChartNoAxesCombined, Menu } from 'lucide-react'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { ThemeToggle } from '@features/theme-toggle'
@@ -10,16 +11,21 @@ import { Button, Sheet, SheetContent, SheetTrigger } from '@shared/ui'
 
 export const Sidebar = () => {
   const pathname = useLocation().pathname
+  const [isOpen, setIsOpen] = useState(false)
   const { t } = useTranslation()
 
   return (
-    <Sheet >
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger>
-        <Button size="icon" variant="ghost">
+        <Button
+          size="icon"
+          variant="ghost"
+          onClick={() => setIsOpen((prevState) => !prevState)}
+        >
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent  side="left">
+      <SheetContent side="left">
         <div className="flex h-full flex-col justify-between">
           <nav className="flex flex-col gap-4 pt-4">
             <Link to="/">
@@ -27,6 +33,7 @@ export const Sidebar = () => {
                 className="w-full justify-start"
                 variant={pathname === basePath ? 'secondary' : 'ghost'}
                 size="lg"
+                onClick={() => setIsOpen(false)}
               >
                 <BookUser />
                 {t('Customers')}
@@ -39,6 +46,7 @@ export const Sidebar = () => {
                   pathname === `${basePath}stats` ? 'secondary' : 'ghost'
                 }
                 size="lg"
+                onClick={() => setIsOpen(false)}
               >
                 <ChartNoAxesCombined />
                 {t('Statistics')}
